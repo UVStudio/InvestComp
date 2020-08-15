f80c9ddb94f660822fd0b9f2424104c820928f70
Leonard Shen <uvstudio2199@gmail.com>
2020-08-15 19:45:40 -0400

    added security libraries

--- server.js
+++ server.js
@@ -4,12 +4,30 @@ const path = require('path');
 const connectDB = require('./config/db');
 const methodOverride = require('method-override');
 
+//security libraries
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 app.use(methodOverride('_method'));
 
 connectDB();
 
 //init middleware to allow us to get data in req.body, or else we get undefined
 app.use(express.json({ extended: false }));
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+// //Prevents cross-site scripting attacks
+app.use(xss());
+
+//Prevent http param pollution
+app.use(hpp());
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
